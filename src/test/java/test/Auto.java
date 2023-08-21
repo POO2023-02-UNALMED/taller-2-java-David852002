e9080efcc423a4761707abc95b3ad08c0da7e66c
David852002 <ddelgadoo@unal.edu.co>
2023-08-21 18:48:05 -0500

    clase auto

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,36 @@
+package test;
+
+public class Auto {
+	public String modelo;
+	public int precio;
+	public Asiento[] asientos;
+	public String marca;
+	public static int registro;
+	public int cantidadCreados;
+	
+		
+	
+	public int cantidadAsientos() {
+	return asientos.length;	
+	}
+	
+	public void verificarIntegridad() {
+		if (Auto.registro == Asiento.registro && 
+			Auto.registro == Motor.registro) {
+			
+			System.out.println("Auto original");
+		}
+		
+		else{
+			
+			System.out.println("Las piezas no son originales");
+			
+			}
+		
+			
+			
+			
+		}
+	}
+	
+
