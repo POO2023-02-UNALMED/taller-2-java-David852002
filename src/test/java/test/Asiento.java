d3527c270afc39f15a4c7673e465212b8a77e78b
David852002 <ddelgadoo@unal.edu.co>
2023-08-21 18:39:07 -0500

    clase asiento

---
+++ src/test/java/test/Asiento.java
@@ -0,0 +1,26 @@
+package test;
+
+public class Asiento {
+	
+	public String color;
+	public int precio;
+	public static  int registro;
+
+	public Asiento(String ncolor, int nprecio, int nregistro) {
+		this.color = ncolor;
+		this.precio = nprecio;
+		this.registro = nregistro;
+		
+	}
+
+	
+	public void CambiarColor(String color ) {		
+		if(color=="rojo" || color=="verde" || color=="amarillo" 
+		|| color=="negro" || color=="blanco"	) 
+		
+		{
+			this.color = color;
+		}
+			
+	}
+}
